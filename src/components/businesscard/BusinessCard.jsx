bab0ac923338b514680decd40f84c33f05d07baf
Fatih Durdu <mfdurdu@gmail.com>
2023-08-11 00:44:51 +0300

    File changed

--- src/components/businesscard/BusinessCard.jsx
+++ src/components/businesscard/BusinessCard.jsx
@@ -13,18 +13,23 @@ import {
 
 function save() {
   const fileData = `BEGIN:VCARD
-VERSION:4.0
-N:Fatih Durdu;
-FN:Fatih Durdu
-ORG:Software Co.
-TITLE:Software Engineer
-TEL;TYPE#work,voice;VALUE#uri:tel:+905423875430
-TEL;TYPE#home,voice;VALUE#uri:tel:+905307746114
-ADR;TYPE#WORK;PREF#1;LABEL#"Normality\nYeni, Sait Girgin Cd., 34570 Silivri/İstanbul
-EMAIL:mfdurdu@gmail.com
-REV:20080424T195243Z
-x-qq:21588891
-END:VCARD
+  VERSION:2.1
+  N:Gump;Forrest;;Mr.
+  FN:Forrest Gump
+  ORG:Bubba Gump Shrimp Co.
+  TITLE:Shrimp Man
+  PHOTO;GIF:http://www.example.com/dir_photos/my_photo.gif
+  TEL;WORK;VOICE:(111) 555-1212
+  TEL;HOME;VOICE:(404) 555-1212
+  ADR;WORK;PREF:;;100 Waters Edge;Baytown;LA;30314;United States of America
+  LABEL;WORK;PREF;ENCODING#QUOTED-PRINTABLE;CHARSET#UTF-8:100 Waters Edge#0D#
+   #0ABaytown\, LA 30314#0D#0AUnited States of America
+  ADR;HOME:;;42 Plantation St.;Baytown;LA;30314;United States of America
+  LABEL;HOME;ENCODING#QUOTED-PRINTABLE;CHARSET#UTF-8:42 Plantation St.#0D#0A#
+   Baytown, LA 30314#0D#0AUnited States of America
+  EMAIL:forrestgump@example.com
+  REV:20080424T195243Z
+  END:VCARD
   `;
 
   const blob = new Blob([fileData], { type: "text/plain" });
