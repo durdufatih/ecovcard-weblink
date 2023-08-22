4bc12d50a61f7606157441fff5e780d2c59e0035
Fatih Durdu <mfdurdu@gmail.com>
2023-08-22 09:44:24 +0300

    Some changes for see of card detail

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,24 +2,23 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "../App.css";
 import BusinessCard from "../components/businesscard/BusinessCard";
-import backgroundImage from "../images/backgroung.jpg";
-import logo from "../images/logo.svg";
 import { db } from "../config/Firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
 
 function HomePage() {
   let { id } = useParams();
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    console.log("Hello")
-    const cardRef = doc(db, 'cards', id); // Assuming 'db' is your Firestore instance
+    console.log("Hello");
+    const cardRef = doc(db, "cards", id); // Assuming 'db' is your Firestore instance
     getDoc(cardRef)
       .then((docSnapshot) => {
         if (docSnapshot.exists()) {
           const cardData = docSnapshot.data();
           setData(cardData);
-          console.log(cardData.selectedImageUrl)
+          console.log(cardData.selectedImageUrl);
           console.log("Card data:", cardData);
         } else {
           console.log("Card not found");
@@ -28,15 +27,21 @@ function HomePage() {
       .catch((error) => {
         console.error("Error fetching card:", error);
       });
+    saveUserAction();
   }, [id]);
+
+  async function saveUserAction() {
+    await setDoc(doc(db, "views", uuidv4()), {
+      actualTime: new Date(),
+      id: id,
+      action: "view",
+    });
+  }
   return (
     <div className="App">
       <div className="card">
         <div className="card-content">
-          <div>{id}</div>
-          <BusinessCard
-            card = {data}
-          />
+          <BusinessCard card={data} />
         </div>
       </div>
     </div>
